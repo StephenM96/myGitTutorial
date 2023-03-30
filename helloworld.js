f12fd562a9b576cad4b547d4d1a969ebcc9a4a31
unknown <mossysm96@gmail.com>
2023-03-29 21:01:27 -0500

    tested functions

--- helloworld.js
+++ helloworld.js
@@ -1,62 +1,91 @@
-const Sentiment = require("sentiment")
+// const Sentiment = require("sentiment")
 
-const sentimentPackage = new Sentiment();
+// const sentimentPackage = new Sentiment();
 
-const result = sentimentPackage.analyze("Cats are very cool.");
+// const result = sentimentPackage.analyze("Cats are very cool.");
 
-console.log(result); //Score: -2, Comparative: -0.666
+// console.log(result); //Score: -2, Comparative: -0.666
 
-function SayHello(variable, variable_2){
-    console.log(variable, variable_2)
-}
+// function SayHello(variable, variable_2){
+//     console.log(variable, variable_2)
+// }
 
-SayHello("Hack the Planet", "Hello World")
+// SayHello("Hack the Planet", "Hello World")
 
-function SUM(A, B){
-    return A + B
-}
+// function SUM(A, B){
+//     return A + B
+// }
 
-console.log(SUM(2,2))
+// console.log(SUM(2,2))
 
-function MULT(A, B){
-    return A * B
-}
+// function MULT(A, B){
+//     return A * B
+// }
 
-console.log(MULT(3,4))
+// console.log(MULT(3,4))
 
-function SUB(A, B){
-    return A - B
-}
+// function SUB(A, B){
+//     return A - B
+// }
 
-console.log(SUB(3,4))
+// console.log(SUB(3,4))
 
-function DIV(A, B){
-    return A / B
-}
+// function DIV(A, B){
+//     return A / B
+// }
 
-console.log(DIV(3,4))
+// console.log(DIV(3,4))
 
-function Stephen(variable_3, variable_4){
-    console.log("Hello", variable_4, "-", variable_3)
-}
+// function Stephen(variable_3, variable_4){
+//     console.log("Hello", variable_4, "-", variable_3)
+// }
 
-Stephen("Stephen", "Moss")
+// Stephen("Stephen", "Moss")
 
-function isCool(name){
-    if(name === 'Stephen'){
-      return true
-    } else {
-      return false
-    }
-  }
+// function isCool(name){
+//     if(name === 'Stephen'){
+//       return true
+//     } else {
+//       return false
+//     }
+//   }
   
-  function checkCoolness(name){
-    const cool = isCool(name) // true
-    if (cool === true){
-      console.log(name, 'is cool!')
-    } else { //false
-      console.log(name, 'is not cool.')
-    }
-  }
+//   function checkCoolness(name){
+//     const cool = isCool(name) // true
+//     if (cool === true){
+//       console.log(name, 'is cool!')
+//     } else { //false
+//       console.log(name, 'is not cool.')
+//     }
+//   }
   
-  checkCoolness('Stephen')
+//   checkCoolness('Stephen')
+
+//   const cars = ["Hyundai", "Honda", "Ford", "Nissan", "Toyota"];
+//   console.log(cars)
+//   cars.shift("Hyundai");
+//   console.log(cars)
+//   cars.unshift("Chevrolet");
+//   console.log(cars)
+//   cars[3] = "Dodge";
+//   console.log(cars)
+
+// const book = {
+//   title : "The Old Man and the Sea",
+//   author: {
+//     firstName: "Ernest",
+//     lastName: "Hemingway",
+//   },
+//   numberOfPages : 127,
+//   shortDescription: "The Old Man and the Sea is a novella written by the American author Ernest Hemingway in 1951 in Cayo Blanco (Cuba), and published in 1952. It was the last major work of fiction written by Hemingway that was published during his lifetime. One of his most famous works, it tells the story of Santiago, an aging Cuban fisherman who struggles with a giant marlin far out in the Gulf Stream off the coast of Cuba.",
+// }
+//   console.log(book)
+
+// function add(a,b){
+//   return (a*b)
+// }
+
+// if(add(1,5)!=6){
+//   throw Error
+//   }
+// add(1,2)
